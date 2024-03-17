355df572a31315848384640ed42580c99f5c1633
codingcreedd <moubayedmarwancoding@gmail.com>
2024-03-17 14:09:08 +0200

    Create gameboard basic logic

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,8 +1,23 @@
 class Gameboard{
-
-    
-
-    placeShips(){
-        
+    constructor(){
+        this.ships = [];
+        this.grid = Array.from({ length: 10 }, () => Array(10).fill(null)); //10x10 Array
     }
+
+    placeShip(ship, row, col, orientation){
+        for(let i = 0, coordinate = 0; i < ship.length; i++){
+            if(orientation === 'horizontal')
+            {
+                coordinate = col;
+
+            }
+            else if(orientation === 'vertical')
+            {
+                coordinate = row;
+            }
+        }
+    }
+
 }
+
+module.exports = Gameboard;
