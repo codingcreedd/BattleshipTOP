408be7643608ce7c513d54963508272a3fa375c9
codingcreedd <moubayedmarwancoding@gmail.com>
2024-03-17 17:49:32 +0200

    Add render functions to render both boards

--- src/DOM.js
+++ src/DOM.js
@@ -1,4 +1,4 @@
-(function DOM(){
+function DOM(){
 
     const renderBoards = board => {
         const boardContainer = document.querySelector(`.${board}`);
@@ -10,4 +10,39 @@
         }
     }    
 
-})();
+    const setShipBoard = () => {
+        const setterBoardContainer = document.querySelector('.board-select-wrapper');
+        setterBoardContainer.classList.remove('hidden');
+
+        const setterBoard = setterBoardContainer.querySelector('.board');
+        for(let i = 1; i <= 100; i++)
+        {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            setterBoard.appendChild(cell);
+        }
+
+        const startGameButton = document.querySelector('.start-button');
+        startGameButton.addEventListener('click', () => {
+            setterBoardContainer.classList.add('hidden');
+            copyShipsIntoMainBoard();
+        });
+
+        
+
+    }
+
+
+
+    return{
+        renderBoards,
+        setShipBoard
+    }
+
+};
+
+const dom = new DOM();
+dom.renderBoards('player-board');
+dom.renderBoards('computer-board');
+dom.setShipBoard();
+
