a7b3f94c23b7bb7130853c818a3636837699e228
codingcreedd <moubayedmarwancoding@gmail.com>
2024-03-19 03:42:52 +0200

    Add computer functionality in Player.js

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,34 @@
 export default class Player{
     constructor(){
-        this.player_turn = true; 
+        this.computer_turn = false; 
     }
+
+    #getRandomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+      
+
+    #computerPickCoordinates(computerBoard){
+        const row = 0, col = 0;
+        for(let i = 1; i <= 5; i++){
+            while(true){
+                row = this.#getRandomNumber(0, 9);
+                col = this.#getRandomNumber(0,9);
+                if(computerBoard.grid[row][col] === null)
+                    break;
+            }
+        }
+
+        return {row, col};
+    }
+
+    computerPlaceShips(computerBoard, shipSize){
+        const ship = new Ship(shipSize);
+        const orientation = (this.#getRandomNumber(1, 2) === 1) ? 'horizontal' : 'vertical';
+
+        const computerCoordinates = this.#computerPickCoordinates(computerBoard);
+
+        computerBoard.placeShip(ship, computerCoordinates.row, computerCoordinates.col, orientation, e, 'computer');
+    }
+
 }
